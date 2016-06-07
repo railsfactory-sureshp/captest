3cd5bf147c8339be9657ed2a8a3e8e91062f1d7e
railsfactory-pavankumarc <pavankumar@railsfactory.org>
2016-06-07 16:44:09 +0530

    first commit

---
+++ captest/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Captest::Application.config.secret_key_base = '6107396a91d7705fed4871e6c74c1cb115f5caa3f17f02f96f5dd25c9726d6ae3516900f309b022107aaf664eb0c801c1150447e44556c1c6e0f05184e2fe113'
---
+++ captest/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Captest::Application.config.session_store :cookie_store, key: '_captest_session'
